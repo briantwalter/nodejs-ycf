78bc92994ff786b946cbb3636e8973220136abde
briantwalter <walter.brian@gmail.com>
2014-03-14 11:48:06 -0700

    start

---
+++ ycf.js
@@ -0,0 +1,20 @@
+//
+// ycf.js
+//
+
+// variables
+var rest_client = require('node-rest-client').Client;
+var soap_client = require('node-soap-client').Client;
+var express = require('express)';
+
+// main
+
+
+
+
+
+
+
+// start daemon process
+//require('daemon');
+//console.log("Daemon started with PID: " + process.pid);
