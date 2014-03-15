887a01636f9574fc1bdeb99cdeede43852e6a8ef
briantwalter <walter.brian@gmail.com>
2014-03-14 23:17:29 -0700

    broken

--- ycf.js
+++ ycf.js
@@ -3,15 +3,32 @@
 //
 
 // variables
-var rest_client = require('node-rest-client').Client;
-var soap_client = require('node-soap-client').Client;
-var express = require('express)';
+var rest = require('node-rest-client').Client;
+var soap = require('soap');
+var express = require('express');
+var catsapi = 'http://catfacts-api.appspot.com/api/facts';
+var yodaapi = 'http://www.yodaspeak.co.uk/webservice/yodatalk.php?wsdl';
 
 // main
-
-
-
-
+var app = express();
+// get the cat fact itself
+var restclient = new rest();
+restclient.registerMethod("jsonMethod", catsapi, "GET");
+restclient.methods.jsonMethod(function(request, response) {
+  // check api call
+  console.log("DEBUG: made REST call on: " + catsapi);
+  var catfact = JSON.parse(request);
+  // check object for a single cat fact
+  console.log("DEBUG: " + catfact.facts);
+});
+// translate the fact to yoda speak
+var args = {inputText: "Cats have 9 lives."};
+console.log("DEBUG: args are " + args);
+soap.createClient(yodaapi, function(error, client) {
+  client.yodaTalkRequest(args, function(err, result) {
+    console.log("DEBUG: " + result);
+  });
+});
 
 
 
