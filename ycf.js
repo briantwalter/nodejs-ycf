4dd706a57f93d2c8df53689e8c2eb13183663544
briantwalter <walter.brian@gmail.com>
2014-03-31 11:11:17 -0700

    Someting Wong

--- ycf.js
+++ ycf.js
@@ -10,54 +10,56 @@ var port = '8800';
 var catsapi = 'http://catfacts-api.appspot.com/api/facts';
 var yodaapi = 'http://www.yodaspeak.co.uk/webservice/yodatalk.php?wsdl';
 
+// functions
+// build and show the catfact page
+function showpage() {
+  var restclient = new rest();
+  restclient.registerMethod("jsonMethod", catsapi, "GET");
+  restclient.methods.jsonMethod(function (request, response) {
+    // check cats api call
+    console.log("DEBUG: made REST call on: " + catsapi);
+    var catfact = JSON.parse(request);
+    // check object for a single cat fact
+    console.log("DEBUG: CATS: " + catfact.facts);
+    // construct soap payload
+    var args = {inputText: catfact.facts};
+    console.log("DEBUG: made SOAP call on: " + yodaapi);
+    // create soap client for yoda translation
+    soap.createClient(yodaapi, function(err, client) {
+      if (err) {
+       console.log("DEBUG: there was a problem with the soap url");
+      };
+      client.yodaTalk(args, function(err, result) {
+        console.log("DEBUG: YODA: " + result.return);
+        var catfact = result.return;
+        // create and display the page
+        app.get('/', function(req, res) {
+          res.render('index',
+          { 
+            title: "my title",
+            catfact: catfact,
+            myipaddr: "10.0.1.1" 
+          }
+          )
+        })
+      });
+    });
+  });
+}
+
 // main
 var app = express();
-
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(app.router);
 app.use(express.logger('dev'))
 app.use(express.static(__dirname + '/html'))
 app.use(express.errorHandler());
-
-// create and display the page
-app.get('/', function(req, res) {
-  res.render('index',
-  { 
-    title: "my title",
-    catfact: "my cat fact", 
-    myipaddr: "10.0.1.1" 
-  }
-  )
-})
+// build and show page
+showpage();
 
 // start the http server
 app.listen(port)
-
-// get the cat fact itself
-//var restclient = new rest();
-//restclient.registerMethod("jsonMethod", catsapi, "GET");
-//restclient.methods.jsonMethod(function (request, response) {
-  // check cats api call
-//  console.log("DEBUG: made REST call on: " + catsapi);
- // var catfact = JSON.parse(request);
-  // check object for a single cat fact
-//  console.log("DEBUG: CATS: " + catfact.facts);
-  // construct soap payload
-//  var args = {inputText: catfact.facts};
-//  console.log("DEBUG: made SOAP call on: " + yodaapi);
-  // create soap client for yoda translation
-//  soap.createClient(yodaapi, function(err, client) {
-//    if (err) {
-//     console.log("DEBUG: there was a problem with the soap url");
-//    };
-//    client.yodaTalk(args, function(err, result) {
-//      console.log("DEBUG: YODA: " + result.return);
-//    });
-//  });
-//});
-
-
 
 // start daemon process
 //require('daemon');
